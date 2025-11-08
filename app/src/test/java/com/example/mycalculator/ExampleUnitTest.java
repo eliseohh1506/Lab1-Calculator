004a80ff02a2cb1acfa7e763792013cc08a2751c
Judy Diep <judydiep@Judys-MacBook-Air.local>
2025-11-07 20:22:58 -0500

    Initial commit

--- app/src/test/java/com/example/mycalculator/ExampleUnitTest.java
+++ app/src/test/java/com/example/mycalculator/ExampleUnitTest.java
@@ -1,22 +1,21 @@
-package com.example.mycalculator;
-
-import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        MathEval e = new MathEval();
-        BigDecimal actual = e.eval("2+2").setScale(2, BigDecimal.ROUND_HALF_UP);
-        // comparing Big decimal to Big Decimal value to ensure same precision during comparison of actual and expected values
-        assertEquals("Error, Addition should return 4",new BigDecimal("4.00"), actual);
-    }
-}
+    /**
+     * Example local unit test, which will execute on the development machine (host).
+     *
+     * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+     */
+    public class ExampleUnitTest {
+        @Test
+        public void addition_isCorrect() {
+            assertEquals(4, 2 + 2);
+            public void addition_isCorrect() throws Exception {
+                MathEval e = new MathEval();
+                BigDecimal actual = e.eval("2+2").setScale(2, BigDecimal.ROUND_HALF_UP);
+                // comparing Big decimal to Big Decimal value to ensure same precision during comparison of actual and expected values
+                assertEquals("Error, Addition should return 4",new BigDecimal("4.00"), actual);
+            }
+        }
