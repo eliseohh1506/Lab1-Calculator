7172039103a0526a5347369c4d0b916bacf2f612
Judy Diep <judydiep@Judys-MacBook-Air.local>
2025-11-10 04:00:07 -0500

    Initial commit

--- app/src/test/java/com/example/mycalculator/ExampleUnitOneTest.java
+++ app/src/test/java/com/example/mycalculator/ExampleUnitOneTest.java
@@ -1,21 +1,24 @@
-
+package com.example.mycalculator;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 
     /**
      * Example local unit test, which will execute on the development machine (host).
      *
      * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
      */
-    public class ExampleUnitTest {
+    public class ExampleUnitOneTest {
         @Test
-        public void addition_isCorrect() {
-            assertEquals(4, 2 + 2);
-            public void addition_isCorrect() throws Exception {
-                MathEval e = new MathEval();
-                BigDecimal actual = e.eval("2+2").setScale(2, BigDecimal.ROUND_HALF_UP);
+        public void addition_isCorrect() throws Exception {
+                assertEquals(4, 2 + 2);
+
+                BigDecimal actual = MathEval.eval("2+2").setScale(2, RoundingMode.HALF_UP);
+
                 // comparing Big decimal to Big Decimal value to ensure same precision during comparison of actual and expected values
                 assertEquals("Error, Addition should return 4",new BigDecimal("4.00"), actual);
-            }
         }
+    }
